ec09876609a06d1bb582a3d84723d3765d5af2e9
nakano1122 <akito2998@gmail.com>
2025-10-17 23:40:01 +0900

    add sitemap setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import { defineConfig } from "astro/config";
 
 import icon from "astro-icon";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://nakano1122.github.io/",
-  integrations: [
-    icon({
-      iconDir: "node_modules/@iconify/json",
-    }),
-  ],
+  integrations: [icon({
+    iconDir: "node_modules/@iconify/json",
+  }), sitemap()],
 });
