18a443177e46980198c27a7c9dcf3de87bd71383
nakano1122 <akito2998@gmail.com>
2026-03-22 18:42:07 +0900

    fix mail address

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   site: "https://nakano1122.github.io/",
   integrations: [icon({
     include: {
-      mdi: ['weather-sunny', 'weather-night', 'email-outline', 'github', 'open-in-new', 'close'],
+      mdi: ['weather-sunny', 'weather-night', 'email-outline', 'github', 'open-in-new', 'close', 'at'],
     },
   }), sitemap()],
 });
